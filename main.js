6e73f39c7f480f913941a72f1f892e1446679899
ahmedzg1 <89781515+ahmedzg1@users.noreply.github.com>
2021-09-15 13:36:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById("btn1").onclick = function(){
+            this.style.backgroundColor ="red";
+};
+
+document.getElementById("btn2").onclick = function(){
+            this.style.backgroundColor ="red";
+        };
+
+        document.getElementById("btn3").onclick = function(){
+            this.style.backgroundColor ="red";
+        };
+        function increment() {
+      document.getElementById("quant1").value++;
+   }
+
+   function decrement() {
+      document.getElementById("quant1").stepDown();
+   }
+
+
